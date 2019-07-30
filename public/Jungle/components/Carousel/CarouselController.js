06d09ef2b81e4db0dbabd5b347c37c1a9ce04e00
fed-gren <deveb22@gmail.com>
2019-07-30 13:40:00 +0900

    fix: link address
    - heroku 배포 주소로 수정

--- public/Jungle/components/Carousel/CarouselController.js
+++ public/Jungle/components/Carousel/CarouselController.js
@@ -1,7 +1,6 @@
 import MyFetch from "../../../Grenutil/MyFetch/index.js";
 
-const FETCH_PATH = "http://localhost:8080/data/localData.json";
-// "https://fed-gren.github.io/amazon-clone/data/localData.json";
+const FETCH_PATH = "https://gren-amazon.herokuapp.com/localData.json";
 
 const CSS_PATH = "Jungle/components/Carousel/Carousel.css";
 
